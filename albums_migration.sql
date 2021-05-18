aa3d74a366e01b27e2c6ae823e1d2ee7f999bdda
Kenyon Luce <kenyon.luce02@gmail.com>
2021-05-18 12:07:50 -0500

    created albums table

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,1 +1,13 @@
 USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(26) NOT NULL,
+    name  VARCHAR(26) NOT NULL,
+    release_date INT UNSIGNED NOT NULL,
+    sales FLOAT UNSIGNED NOT NULL,
+    genre VARCHAR(26) NOT NULL,
+    PRIMARY KEY (id)
+);
