09c3d0be7396c454d40955ec2a3e021d9363b9d4
Kenyon Luce <kenyon.luce02@gmail.com>
2021-05-19 12:49:37 -0500

    updated update exercise

--- update_exercises.sql
+++ update_exercises.sql
@@ -12,18 +12,32 @@
 
 USE codeup_test_db
 
-SELECT 'All albums from table (sales times 10)';
-SELECT * FROM albums;
+SELECT 'All albums from table';
+SELECT * FROM albums; --before
+
+SELECT 'All albums from table (change sales times 10)'
 UPDATE albums
 SET sales = sales * 10;
+SELECT * FROM albums; --after
 
-SELECT 'All albums released before 1980 (turn into 1800s)';
-SELECT * FROM albums WHERE release_date < 1980;
+
+SELECT 'All albums released before 1980';
+SELECT * FROM albums WHERE release_date < 1980; --before
+
+SELECT 'All albums released before 1980 (change into 1800s)';
 UPDATE albums
 SET release_date = release_date - 100;
 WHERE release_date < 1980;
+SELECT * FROM albums WHERE release_date < 1980; --after
+
 
 SELECT 'All albums by Michael Jackson';
-SELECT * FROM albums WHERE artist = 'Michael Jackson';
+SELECT * FROM albums WHERE artist = 'Michael Jackson'; --before
+
+SELECT 'All albums by Michael Jackson (change to Peter Jackson)';
+UPDATE albums
+SET artist = 'Peter Jackson'
+WHERE artist = 'Michael Jackson'
+SELECT * FROM albums WHERE artist = 'Michael Jackson'; --after
 
 -- mysql -u codeup_test_user -p < update_exercises.sql;
