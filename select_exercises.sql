a0af73d54664a6e02b0213df31cee4e5639d703f
Kenyon Luce <kenyon.luce02@gmail.com>
2021-05-19 10:26:23 -0500

    updataed select exercise

---
+++ select_exercises.sql
@@ -0,0 +1,26 @@
+-- In select_exercises.sql write queries to find the following information. Before each item, output a caption explaining the results:
+--
+-- The name of all albums by Pink Floyd.
+-- The year Sgt. Pepper's Lonely Hearts Club Band was released
+-- The genre for Nevermind
+-- Which albums were released in the 1990s
+-- Which albums had less than 20 million certified sales
+-- All the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?
+
+SELECT 'The name of all albums by Pink Floyd';
+SELECT name FROM albums WHERE artist = 'Pink Floyd';
+
+SELECT 'The year Sgt. Pepper''s Lonely Hearts Club Band was released';
+SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+
+SELECT 'The genre for Nevermind';
+SELECT genre FROM albums WHERE name = 'Nevermind';
+
+SELECT 'Which albums were released in the 1990s';
+SELECT name FROM albums WHERE release_date > 1990;
+
+SELECT 'Which albums had less than 20 million certified sales';
+SELECT name FROM albums WHERE sales < 20.0;
+
+SELECT 'All the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?';
+SELECT name FROM albums WHERE genre = 'Rock';
