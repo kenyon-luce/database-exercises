59e65430f4d24bba42be7509ca2a7ca99093768b
Kenyon Luce <kenyon.luce02@gmail.com>
2021-05-20 11:26:42 -0500

    fixed issues, completed part 2

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,13 +1,19 @@
 USE employees;
 
-SELECT * FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya' AND gender = 'M';
-/*^includes Fs*/
+SELECT * FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') AND gender = 'M';
+/*
+^^includes Fs
+update: fixed by wrapping all my OR queries with parenthesis
+*/
 
 SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
 
 SELECT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
 
-SELECT * FROM employees WHERE last_name !LIKE '%q%';
-/*how to get no '%qu%'?*/
+SELECT * FROM employees WHERE last_name LIKE '%Q%' AND last_name NOT LIKE '%qu%';
+/*
+how to get no '%qu%'?
+update: fixed using NOT LIKE query
+*/
 
 /*mysql -u codeup_test_user -p < where_exercises.sql;*/
