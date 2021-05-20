4d0e51b58d5107f01459a6092dc14afb7da8525a
Kenyon Luce <kenyon.luce02@gmail.com>
2021-05-20 11:14:29 -0500

    working on part 2 wip

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,9 +1,13 @@
 USE employees;
 
-SELECT * FROM employees WHERE first_name IN('Irena', 'Vidya', 'Maya');
+SELECT * FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya' AND gender = 'M';
+/*^includes Fs*/
 
-SELECT * FROM employees WHERE last_name LIKE 'E%';
+SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
 
-SELECT * FROM employees WHERE last_name LIKE '%q%';
+SELECT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
+
+SELECT * FROM employees WHERE last_name !LIKE '%q%';
+/*how to get no '%qu%'?*/
 
 /*mysql -u codeup_test_user -p < where_exercises.sql;*/
