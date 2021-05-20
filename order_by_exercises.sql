498834d5cf70de6bfcbee77175319bd04f4d945d
Kenyon Luce <kenyon.luce02@gmail.com>
2021-05-20 14:35:33 -0500

    updating order exercise

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,6 +1,6 @@
 USE employees;
 
-SELECT * FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') AND gender = 'M' ORDER BY last_name, first_name;
+SELECT * FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') ORDER BY last_name, first_name;
 
 SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%E' ORDER BY emp_no;
 
