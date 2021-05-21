a19207c07526ed078d18d6010202e3ced47be8bd
Kenyon Luce <kenyon.luce02@gmail.com>
2021-05-21 11:02:40 -0500

    ordered days worked

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -8,9 +8,7 @@ SELECT * FROM employees WHERE month(birth_date) = 12 AND day(birth_date) = 25;
 
 SELECT * FROM employees WHERE month(birth_date) = 12 AND day(birth_date) = 25 AND (year(hire_date) BETWEEN 1990 AND 2000) ORDER BY birth_date ASC, hire_date DESC;
 
-
-
-SELECT *, datediff(curdate(), hire_date) AS days_worked FROM employees WHERE month(birth_date) = 12 AND day(birth_date) = 25 AND (year(hire_date) BETWEEN 1990 AND 2000);
+SELECT *, datediff(curdate(), hire_date) AS days_worked FROM employees WHERE month(birth_date) = 12 AND day(birth_date) = 25 AND (year(hire_date) BETWEEN 1990 AND 2000) ORDER BY days_worked;
 
 
 -- SELECT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E' ORDER BY emp_no DESC;
