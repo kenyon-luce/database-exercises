5a680998ddd8850a8eeff2a0c2e7d611887f337f
Kenyon Luce <kenyon.luce02@gmail.com>
2021-05-21 12:28:36 -0500

    updated group by

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -4,4 +4,4 @@ SELECT first_name, last_name FROM employees WHERE last_name LIKE '%E' AND last_n
 
 SELECT count(last_name) AS occurrences, last_name FROM employees WHERE last_name LIKE '%Q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name;
 
-SELECT count(gender) AS count, gender FROM employees WHERE first_name IN ('irena', 'vidya', 'maya') GROUP BY gender;
+SELECT count(gender) AS count, gender FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') GROUP BY gender;
