7bad969e2742c8ef58a107ffc77a750f681cd727
Kenyon Luce <kenyon.luce02@gmail.com>
2021-05-22 17:01:42 -0500

    figured it out, updating join exercise

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,10 +1,18 @@
 USE employees;
 
-SELECT * FROM dept_manager WHERE year(to_date)= 9999;
+-- write a query that shows each department along with the name of the current manager for that department.
+/*
+current_dept_emp --> emp_no, dept_no, from_date, to_date
+departments --> dept_no, [dept_name]
+dept_emp --> emp_no, dept_no, from_date, to_date
+dept_manager --> emp_no, dept_no, from_date, to_date
+employees --> emp_no, birth_date, [first_name, last_name], gender
+*/
 
-SELECT concat(first_name, ' ', last_name) AS manager FROM employees JOIN dept_manager ON employees.emp_no = dept_manager.emp_no WHERE year(to_date)= 9999;
---^gets current manager (without specifying department names)
+SELECT * FROM dept_manager WHERE year (to_date)= 9999; --returns manager emp_no along with their dept_no (no names)
+
+SELECT * FROM departments;
 
 -- need department name and current manager for each department. I can only get department name from departments.dept_name and employee names from employees.name, its asking for managers only so I will need to connect it to dept_manager (like the code above), then fetch their dept_id which I will have to connect back to departments to get the name...geez
-SELECT concat(first_name, ' ', last_name) AS manager,  FROM employees JOIN dept_manager ON employees.emp_no = dept_manager.emp_no;
+SELECT departments.dept_name, concat(first_name, ' ', last_name), departments.dept_no FROM employees JOIN dept_manager ON employees.emp_no = dept_manager.emp_no JOIN departments ON dept_manager.dept_no = departments.dept_no WHERE year (to_date)= 9999 ORDER BY dept_no;
 
