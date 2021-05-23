013299d90f06ff973f375f522f6bd2c921571648
Kenyon Luce <kenyon.luce02@gmail.com>
2021-05-23 13:41:00 -0500

    updated join exercise

--- join_exercises.sql
+++ join_exercises.sql
@@ -14,5 +14,6 @@ SELECT * FROM dept_manager WHERE year (to_date)= 9999; --returns manager emp_no 
 SELECT * FROM departments;
 
 -- need department name and current manager for each department. I can only get department name from departments.dept_name and employee names from employees.name, its asking for managers only so I will need to connect it to dept_manager (like the code above), then fetch their dept_id which I will have to connect back to departments to get the name...geez
-SELECT departments.dept_name, concat(first_name, ' ', last_name), departments.dept_no FROM employees JOIN dept_manager ON employees.emp_no = dept_manager.emp_no JOIN departments ON dept_manager.dept_no = departments.dept_no WHERE year (to_date)= 9999 ORDER BY dept_no;
+SELECT departments.dept_name AS department, concat(first_name, ' ', last_name) AS manager, departments.dept_no AS dept_no FROM employees JOIN dept_manager ON employees.emp_no = dept_manager.emp_no JOIN departments ON dept_manager.dept_no = departments.dept_no WHERE year (to_date)= 9999 ORDER BY dept_no;
 
+--JOIN chain. connected employees to dept_manager (through emp_no) to get manager names only, then joined dept_manager to departments (through dept_no) to get department names each employee is in (which we already limited to managers), then we used WHERE to filter to current managers.
