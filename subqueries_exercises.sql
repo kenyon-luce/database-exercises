a31b502dfa143639ccf462e8e71e5e1de9ea7381
Kenyon Luce <kenyon.luce02@gmail.com>
2021-05-24 10:42:56 -0500

    finished sub-queries exercise

---
+++ subqueries_exercises.sql
@@ -0,0 +1,7 @@
+USE employees;
+
+SELECT * FROM employees WHERE hire_date IN(SELECT hire_date FROM employees WHERE emp_no = 101010);
+
+SELECT titles.title, count(title) AS Aamod FROM titles JOIN employees ON employees.emp_no = titles.emp_no WHERE titles.emp_no IN(SELECT emp_no FROM employees WHERE first_name = 'Aamod') GROUP BY titles.title;
+
+SELECT first_name, last_name FROM employees WHERE emp_no IN(SELECT emp_no FROM dept_manager WHERE year(to_date) = 9999 AND gender = 'F');
