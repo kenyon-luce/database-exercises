96e3967fe0b6c5c62607101b98323fcac3305d90
Kenyon Luce <kenyon.luce02@gmail.com>
2021-05-24 13:03:59 -0500

    finished bonuses

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -8,4 +8,13 @@ SELECT first_name, last_name FROM employees WHERE emp_no IN(SELECT emp_no FROM d
 
 --BONUS
 SELECT dept_name FROM departments WHERE dept_no IN(SELECT dept_no FROM dept_manager WHERE year(to_date) = 9999 AND emp_no IN(SELECT emp_no FROM employees WHERE gender = 'F'));
+--^used nested sub query to get department names with female managers
+
+SELECT first_name, last_name FROM employees WHERE emp_no IN(SELECT emp_no FROM salaries WHERE salary IN(SELECT max(salary) FROM salaries));
+--used nested sub query and max() function to get highest paid employee
+
+SELECT first_name, last_name FROM employees JOIN salaries ON(employees.emp_no = salaries.emp_no) ORDER BY salaries.salary DESC LIMIT 1;
+--same code as above using JOIN
+
+
 
